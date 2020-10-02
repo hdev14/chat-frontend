2e0bf81675eaf7b3ef720e8735d9c082cec18c95
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-02 11:31:44 -0300

    feat: add some methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
-function App () {
+let ws: WebSocket
+
+const App: React.FC = () => {
+  const [name, setName] = useState('')
+  const [chatId, setChatId] = useState('')
+
+  function createChat (): void {
+    if (name && chatId) {
+      ws = new WebSocket(`ws://localhost:4141/?name=${name}&id=${chatId}`)
+    }
+  }
+
+  function genarateChatId (): void {
+    setChatId(uuidv4())
+  }
+
+  /*
+    TODO:
+    - Create modal with buttons to create a new chat or get in a new one.
+    - Create methods to connect and send messages
+  */
+
   return (
     <h1>Hellow</h1>
   )
