a8c0a93d3bbc751276ef6e1a79c601960b8421b2
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-03 09:44:34 -0300

    feat: create WebSocketSingleton

---
+++ src/WebSocketSingleton.ts
@@ -0,0 +1,23 @@
+import { MessageData } from './message-data'
+
+class WebSocketSingleton {
+  private wsConnection: WebSocket | null = null
+
+  public connect(url: string): void {
+    this.wsConnection = new WebSocket(url)
+  }
+
+  public disconnect(): void {
+    this.wsConnection?.close()
+  }
+
+  public sendMessage(data: MessageData): void {
+    this.wsConnection?.send(JSON.stringify(data))
+  }
+
+  public addEvent(event: string, fn: EventListener): void {
+    this.wsConnection?.addEventListener(event, fn);
+  }
+}
+
+export default new WebSocketSingleton()
---
+++ src/message-data.ts
@@ -0,0 +1,4 @@
+export type MessageData = {
+  message: string,
+  date: Date
+}
